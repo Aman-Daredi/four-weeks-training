8c7c477d5d2e44d71109153b80eafaf86908f755
Aman Daredi <darediaman@gmail.com>
2023-05-10 12:00:05 +0530

    First Exercise Done.

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            // Create a Circle object and display its area and circumference
+            Circle myCircle = new Circle(10);
+            double areaOfMyCircle = myCircle.GetArea();
+            double circumferenceOfMyCircle = myCircle.GetCircumference();
+
+            Console.WriteLine("Area of my circle is: " + areaOfMyCircle);
+            Console.WriteLine("Circumference of my circle is: " + circumferenceOfMyCircle);
+        }
+    }
+
+    class Circle
+    {
+        double Radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            double area = 3.14 * this.Radius * this.Radius;
+            return area;
+        }
+
+        public double GetCircumference()
+        {
+            double circumference = 2 * 3.14 * this.Radius;
+            return circumference;
         }
     }
 }
