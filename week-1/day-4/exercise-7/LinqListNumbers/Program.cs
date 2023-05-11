a737ef4db6009cc622318fdd97a05748f78894fd
Aman Daredi <darediaman@gmail.com>
2023-05-11 18:43:03 +0530

    Exercise 7 Day 4 Done.

--- week-1/day-4/exercise-7/LinqListNumbers/Program.cs
+++ week-1/day-4/exercise-7/LinqListNumbers/Program.cs
@@ -9,10 +9,25 @@
 
             // Use LINQ to perform the following operations:
             // 1. Find all even numbers
+            var queryForAllEvenNumbers = numbers.Where(x => x % 2 == 0);
+
             // 2. Find all numbers greater than a specific value (e.g., 20)
+            var queryForNumberGreaterThan20 = numbers.Where(x => x > 20);
+
             // 3. Calculate the sum of all numbers
+            int sum = numbers.Sum(x => x);
+            Console.WriteLine(sum);
+
             // 4. Calculate the average of all numbers
+            double average = numbers.Average(x => x);
+            Console.WriteLine(average);
+
             // 5. Find the minimum and maximum values in the list
+            int Minimum = numbers.Min(x => x);
+            int Maximum = numbers.Max(x => x);
+
+            Console.WriteLine(Minimum + "  " + Maximum);
+
         }
     }
 }
