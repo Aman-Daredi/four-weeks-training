cc2aa8fd0522efa7368ce374539e1f146f8c4282
Aman Daredi <darediaman@gmail.com>
2023-05-12 12:38:36 +0530

    Exercise 1 Day 5 Done.

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            FileLogger fl = new FileLogger("aman.txt");
+            fl.Log("My name is Aman\n");
+            fl.Log("I am 22 years old\n");
+            fl.Dispose();
+
+            // After using fl.Dispose(), I wasn't able to write to the same file as my _writer property was disposed successfully.
+
+            fl = new FileLogger("ekta.txt");
+            fl.Log("Hello World");
+            fl.Dispose();
         }
     }
 
@@ -15,16 +25,19 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath);
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            _writer.Dispose();
         }
 
         public void Log(string message)
         {
             // Write message to the file
+            _writer.Write(message);
         }
     }
 }
